d6134d6ae82126342b8a9b3cf5831a33fdf1b8e9
darya-ko00 <dasha.kon.060203@gmail.com>
2025-08-08 19:11:25 +0300

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -29,7 +29,9 @@ def serialize_tag(tag):
 
 def index(request):
 
-    most_popular_posts = []  # TODO. Как это посчитать?
+    post = Post.objects.all()
+    popular_posts = sorted(post, key=get_likes_count)
+    most_popular_posts = popular_posts[-5:]
 
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
@@ -117,3 +119,6 @@ def contacts(request):
     # позже здесь будет код для статистики заходов на эту страницу
     # и для записи фидбека
     return render(request, 'contacts.html', {})
+
+def get_likes_count(post):
+    return post.likes.count()
