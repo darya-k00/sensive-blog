6699d2400c966bb6a4373583bd4894e9d4ea18f0
darya-ko00 <dasha.kon.060203@gmail.com>
2025-08-11 19:43:50 +0300

    move code to reusable QuerySet method.

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
-
+from django.db.models import Count
 
 class Post(models.Model):
     title = models.CharField('Заголовок', max_length=200)
@@ -37,9 +37,19 @@ class Post(models.Model):
         verbose_name_plural = 'посты'
 
 
+class TagQuerySet(models.QuerySet):
+
+    def popular(self):
+        popular_tags = self.annotate(posts_amount=Count(
+            'posts'
+        )).order_by('-posts_amount')
+        return popular_tags
+
+
 class Tag(models.Model):
     title = models.CharField('Тег', max_length=20, unique=True)
-
+    objects = TagQuerySet.as_manager()
+    
     def __str__(self):
         return self.title
 
